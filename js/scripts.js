a7f1ca3de58173c1d75440e67b9a0685ceb61ab1
Nickolas Caimi <10008189@sbschools.org>
2016-04-20 09:22:50 -0400

    addded in/correct messages, animations, and scoreboard

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,23 @@
-var playerName = '';
+// Trivia game Psuedo Code
+
+// Welcome Page with msg "Welcome to Disney Trivia"
+// Directions?
+// Three strikes and you lose.
+// The user should type their name.
+// When user clicks button to submit their name the first question appears
+
+$(document).ready(function() {
+
+  var playerName = '';
+  var playerAnswer1 = '';
+  var answer1 = '1943'
+  var correctCounter = 0;
 
   // Hide Player Name
-  $('#show-player-name').hide();
+  $('#dashboard').hide();
+  $('.question').hide();
+  $('.correct-msg').hide();
+  $('.incorrect-msg').hide();
 
   // Allow user to type name immediately on page load
   $('#player-name').focus();
@@ -9,7 +25,8 @@ var playerName = '';
     // When user submits name, welcome screen disappears
   function removeWelcomeScreen() {
     $('#welcome-screen').detach();
-    $('#show-player-name').show().addClass('animated fadeIn');
+    $('#dashboard').show().addClass('animated bounceIn');
+    $('#question1').show().addClass('animated bounceIn').focus();
   };
 
   // Store player name
@@ -18,6 +35,36 @@ var playerName = '';
     playerName = $('#player-name').val();
     console.log(playerName);
     $('#show-player-name').text(playerName);
-    $('#welcome-screen').addClass('animated fadeOut');
+    $('#welcome-screen').addClass('animated bounceOut');
     setTimeout(removeWelcomeScreen, 1000);
   });
+
+  // Store answer 1
+  $('#question1-btn').on('click', function(e) {
+    e.preventDefault(); // prevents form from submitting to a database
+    playerAnswer1 = $('#input-question1').val().trim().toLowerCase(); // This assigns the user's input to the variable playerAnswer1
+    console.log('The players answer for question 1 is ' + playerAnswer1);
+    // showPlayerAnswer1(); // This first showPlayerAnswer1 function
+    answerCheck1();
+  });
+
+  // Show answers
+  function showPlayerAnswer1() {
+    $('#show-player-answer1').text(playerAnswer1);
+    $('#correct-answer1').text(answer1)
+  };
+
+  function answerCheck1() {
+    if(playerAnswer1 === answer1) {
+      console.log("Player's answer is correct!");
+      $('#correct-msg1').show().addClass('animated bounceIn');
+      correctCounter++;
+      $('#score').text(correctCounter);
+    }
+    else {
+      console.log("Player's answer is incorrect!");
+      $('#incorrect-msg1').show().addClass('animated bounceIn');
+    }
+  };
+
+}); // ready function ends
